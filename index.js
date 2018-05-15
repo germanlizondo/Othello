08425df3754095dcc32f77252266ab9f188efe43
germanlizondo <german9lp@gmail.com>
2018-05-15 13:34:12 +0200

    version con login html

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 var http = require("http");
 var fs = require("fs");
+var querystring = require("querystring");
+var url = require("url");
 
 
 
-var servidor = http.createServer((req,res)=>{
+var server = http.createServer((req,res)=>{
+
+	
+	var pathname = url.parse(req.url).pathname;
+	console.log("Petició per a  " + pathname + " rebuda.");
+	if (pathname == '/') {
+		
 	var html = fs.readFile("./index.html",(err,html)=>{
-		res.writeHead(200,{"Content-Type": "text/html"})
+
+		
+		res.writeHead(200,{"Content-Type" : "text/html; charset=utf-8"})
 		res.write(html);
 		res.end();
 	});
 	
+
+	}else if (pathname == '/login') {
+		
+		var html = fs.readFile("./login.html",(err,html)=>{
+	
+			
+			res.writeHead(200,{"Content-Type" : "text/html; charset=utf-8"})
+			res.write(html);
+			res.end();
+		});
+		
+	
+		}else if (pathname == '/registre') {
+		
+			var html = fs.readFile("./registre.html",(err,html)=>{
+		
+				
+				res.writeHead(200,{"Content-Type" : "text/html; charset=utf-8"})
+				res.write(html);
+				res.end();
+			});
+			
+		
+			}
+
 });
 
 console.log("Server init");
 
-servidor.listen(8888,"192.168.1.76");
+server.listen(8888);
